898272378923d22507468e2af2cf4526604be28a
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-06-07 14:49:27 +0200

    Fix import name

--- bob/pad/face/utils/__init__.py
+++ bob/pad/face/utils/__init__.py
@@ -1,7 +1,7 @@
 from .load_utils import (
     frames, number_of_frames, yield_faces, scale_face, blocks,
-    bbx_cropper, min_face_size_normalizer, color_augmentation, block_generator,
-    the_giant_video_loader)
+    bbx_cropper, min_face_size_normalizer, color_augmentation,
+    blocks_generator, the_giant_video_loader)
 
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
