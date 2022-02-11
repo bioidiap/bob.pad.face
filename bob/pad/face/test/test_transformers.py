b76b8f4ae247da25e1d6608d6ed5e98c066a27d6
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2022-02-11 15:54:53 +0100

    Handle None frames in VideoToFrames

--- bob/pad/face/test/test_transformers.py
+++ bob/pad/face/test/test_transformers.py
@@ -114,3 +114,21 @@ def notest_video_quality_measure():
     assert (features[0][1] == features[-1][1]).all()
     assert (features[0][1][0] - 2.7748559659812599e-05) < 0.000001
     assert (features[0][1][-1] - 0.16410418866596271) < 0.000001
+
+
+def test_video_to_frames():
+    # create a list of samples of videos
+    # make sure some frames are None
+    # call transform and check if the None frames are dropped
+    import bob.pipelines as mario
+    from bob.bio.video import VideoLikeContainer
+    from bob.pad.face.transformer import VideoToFrames
+
+    videos = [[0, 1, 2, None, 3], [None, None, None]]
+    video_container = [VideoLikeContainer(v, range(len(v))) for v in videos]
+    samples = [mario.Sample(v, key=i) for i, v in enumerate(video_container)]
+    frame_samples = VideoToFrames().transform(samples)
+    assert len(frame_samples) == 4
+    assert all(s.key == 0 for s in frame_samples)
+    assert [s.data for s in frame_samples] == [0, 1, 2, 3]
+    assert [s.frame_id for s in frame_samples] == [0, 1, 2, 4]
